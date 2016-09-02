878b5c66d6948f77579735d000ac5a71aec59f7e
ChrisoftheBoyerClan <chrisoftheboyerclan@gmail.com>
2016-09-02 15:57:19 -0400

    Constants File -- Chris
    Added the constants file and added the configure files to the DesktopLauncher

---
+++ core/src/com/thompson/spectrumshooter/util/Constants.java
@@ -0,0 +1,8 @@
+package com.thompson.spectrumshooter.util;
+
+public class Constants
+{
+	public static final String GAME_NAME = "Spectrum Shooter HD";
+	public static final int GAME_WIDTH = 1366;
+	public static final int GAME_HEIGHT = 768;
+}
--- desktop/src/com/thompson/spectrumshooter/desktop/DesktopLauncher.java
+++ desktop/src/com/thompson/spectrumshooter/desktop/DesktopLauncher.java
@@ -3,10 +3,16 @@ package com.thompson.spectrumshooter.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.thompson.spectrumshooter.SpectrumShooter;
+import com.thompson.spectrumshooter.util.Constants;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = Constants.GAME_NAME;
+		config.height = Constants.GAME_HEIGHT;
+		config.width = Constants.GAME_WIDTH;
+
 		new LwjglApplication(new SpectrumShooter(), config);
 	}
 }
