fcc6fb07b21d0c3cb3d0d844f762e8d097a3e2ce
ChrisoftheBoyerClan <chrisoftheboyerclan@gmail.com>
2016-09-08 14:30:28 -0400

    Enemy Class Constructor -- Chris
    Added a constructor to the Enemy class, made it extend the Sprite class, and a getter for the color code.

--- core/src/com/thompson/spectrumshooter/enemy/Enemy.java
+++ core/src/com/thompson/spectrumshooter/enemy/Enemy.java
@@ -1,5 +1,35 @@
 package com.thompson.spectrumshooter.enemy;
 
-public class Enemy
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Enemy objects.
+ *
+ * @author Christopher Boyer
+ */
+public class Enemy extends Sprite
 {
+
+	private int colorCode;	// The color code of this Enemy
+
+	/**
+	 * Create a new Enemy with the given texture and the given color code.
+	 * @param texture		the texture of this Enemy
+	 * @param colorCode		the color code of this Enemy
+	 */
+	public Enemy(Texture texture, int colorCode)
+	{
+		super(texture);
+		this.colorCode = colorCode;
+	}
+
+	/**
+	 * Get the color code of this Enemy
+	 * @return color code of this Enemy
+	 */
+	public int getColorCode()
+	{
+		return this.colorCode;
+	}
 }
