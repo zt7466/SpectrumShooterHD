c107ca9312b3942ade7914f7fd21ccc72f69192e
ChrisoftheBoyerClan <chrisoftheboyerclan@gmail.com>
2016-09-16 11:38:51 -0400

    WorldRenderer and WorldController
    Added the WorldRenderer Class and the WorldController Class.

---
+++ core/src/com/thompson/spectrumshooter/game/WorldController.java
@@ -0,0 +1,11 @@
+package com.thompson.spectrumshooter.game;
+
+/**
+ * Class that handles all of the objects in the world.
+ *
+ * @author Christopher Boyer
+ */
+public class WorldController
+{
+
+}
---
+++ core/src/com/thompson/spectrumshooter/game/WorldRenderer.java
@@ -0,0 +1,12 @@
+package com.thompson.spectrumshooter.game;
+
+/**
+ * Manages rendering all of the world.
+ *
+ * @author Christopher Boyer
+ */
+public class WorldRenderer
+{
+
+
+}
