30eecaaadfd73d05165cff28b87d797410852cf8
Abraham Loscher <abeloscher@gmail.com>
2016-09-21 09:49:39 -0400

    Audio Management - 1 -- Abe
    Random audio management stuff

--- core/src/com/thompson/spectrumshooter/util/Constants.java
+++ core/src/com/thompson/spectrumshooter/util/Constants.java
@@ -12,4 +12,8 @@ public class Constants
 
 	public static final float VIEWPORT_WIDTH = 5.0f;
 	public static final float VIEWPORT_HEIGHT = 5.0f;
+
+
+
+	public static final String SOUND_PATH = "../core/assets/sound/ping.wav";
 }
