8cb2507ebbd2f2e7e65c938cd42b4fe156a0a073
zt7466 <zt7466@ship.edu>
2016-09-26 10:00:42 -0400

    Added a Simple Game Over Screen
    Game Over Screen Added - not finished product but it will do for now. 
    Need a new font to make it better

---
+++ core/src/com/thompson/spectrumshooter/screens/GameOverScreen.java
@@ -0,0 +1,165 @@
+package com.thompson.spectrumshooter.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.thompson.spectrumshooter.SpectrumShooter;
+import com.thompson.spectrumshooter.color.ColorWheel;
+import com.thompson.spectrumshooter.util.Constants;
+
+public class GameOverScreen implements Screen 
+{
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private SpriteBatch batch;	
+	private Stage stage;
+	private LabelStyle titleLabelStyle;
+	private TextButton playAgain;
+	private TextButton mainMenu;
+	
+	private ColorWheel colorWheel;
+	private int currentColorCode;
+	
+	public GameOverScreen(int numberKilled)
+	{
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
+		
+		viewport = new FitViewport(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
+		batch = new SpriteBatch();
+		
+		stage = new Stage(viewport, batch);
+		Gdx.input.setInputProcessor(stage);
+		
+		colorWheel = new ColorWheel();
+		currentColorCode = colorWheel.incrementColorCode(currentColorCode);
+		
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = Constants.GAME_HEIGHT/6;
+		parameter.borderWidth = 3;
+		parameter.borderColor = Color.BLACK;
+		parameter.shadowOffsetX = 2;
+		parameter.shadowOffsetY = 2;
+		
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.local("master_of_break.ttf"));
+		titleLabelStyle = new LabelStyle(generator.generateFont(parameter), new Color(0.255f, 0.412f, 0.882f, 1f));
+		
+		Table mainTable = new Table();
+		mainTable.setFillParent(true);
+		mainTable.add(new Label("Game Over", titleLabelStyle)).row();
+	
+		mainTable.add(new Label( 5 + " Beaten", titleLabelStyle)).row();
+		
+		parameter.size = Constants.GAME_HEIGHT/20;
+		TextButtonStyle style = new TextButtonStyle();
+		style.font = generator.generateFont(parameter);
+		style.fontColor = Color.WHITE;
+		style.downFontColor = Color.BLACK;
+		
+		playAgain = new TextButton("Play Again", style);
+		playAgain.addListener(new ChangeListener()
+		{
+			@Override
+			public void changed(ChangeEvent event, Actor actor) 
+			{
+				SpectrumShooter.getInstance().setScreen(new MainScreen());
+				dispose();
+			}
+		});
+		
+		mainMenu = new TextButton("Main Menu", style);
+		mainMenu.addListener(new ChangeListener()
+		{
+			@Override
+			public void changed(ChangeEvent event, Actor actor) 
+			{
+				SpectrumShooter.getInstance().setScreen(new MenuScreen());
+				dispose();
+			}
+		});
+		
+		mainTable.add(playAgain).row();
+		mainTable.add(mainMenu);
+		
+		stage.addActor(mainTable);
+	}
+	
+	@Override
+	public void render(float delta) 
+	{
+		updateColors();
+		camera.update();
+		stage.draw();
+	}
+	
+	private void updateColors() 
+	{
+		Color newColor = createColor(currentColorCode);	
+		Gdx.gl.glClearColor(newColor.r, newColor.g, newColor.b, newColor.a);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		titleLabelStyle.fontColor = new Color(1 - newColor.r,1 - newColor.g,1 - newColor.b, 1);
+		currentColorCode = colorWheel.incrementColorCode(currentColorCode);	
+	}
+
+	private Color createColor(int currentColorCode2)
+	{
+		return new Color(
+				colorWheel.getRedValue(currentColorCode)/255.0f,
+				colorWheel.getGreenValue(currentColorCode)/255.0f,
+				colorWheel.getBlueValue(currentColorCode)/255.0f,
+				1);
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
